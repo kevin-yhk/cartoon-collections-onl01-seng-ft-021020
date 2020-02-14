f1bc468e4c0868ee30a3529099926e402ddeceaa
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-14 01:42:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,6 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese == 
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end

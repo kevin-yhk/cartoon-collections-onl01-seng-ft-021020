85e9dc9acd4a593bd8406371139bf517298da1d6
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-14 01:34:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def summon_captain_planet(planeteer_calls)
   return calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  
 end
 
 def find_the_cheese# code an argument here
